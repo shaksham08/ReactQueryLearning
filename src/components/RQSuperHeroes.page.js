58b15c56869766ffeaecafb4d5e20b344a3cad1f
shaksham08 <shakshamsinha@outlook.com>
2024-03-03 15:27:16 +0530

    dev:Success and error callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,12 +2,25 @@ import { useQuery } from "react-query"
 import axios from "axios"
 
 const fetchSuperHeroes = () => {
-    return axios.get("http://localhost:4000/superheroes")
+    return axios.get("http://localhost:4000/superheroes1")
 }
 
 export const RQSuperHeroesPage = () => {
-    const { isLoading, data, isError, error, isFetching, refetch } = useQuery("super-heroes", fetchSuperHeroes, {
-        enabled: false
+
+    const onSuccess = (data) => {
+        console.log(
+            "Perform side effect after data fetching", data
+        )
+    }
+
+    const onError = (data) => {
+        console.log(
+            "Perform side effect after encountering error", data
+        )
+    }
+
+    const { isLoading, data, isError, error, isFetching, } = useQuery("super-heroes", fetchSuperHeroes, {
+        onError, onSuccess
     })
     // default cache time is 5 minutes and this 5 minutes is good 
     // default stale time is 0s
@@ -28,7 +41,6 @@ export const RQSuperHeroesPage = () => {
     return (
         <>
             <h2>React Query Super Heroes Page</h2>
-            <button onClick={refetch}>Fetch heroes</button>
             {data?.data.map(hero => {
                 return <div key={hero.name}>{hero.name}</div>
             })}
