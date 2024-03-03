d268d25ea149a77eecdac40bb96332cf14a8bc31
shaksham08 <shakshamsinha@outlook.com>
2024-03-03 16:02:31 +0530

    dev:refetching assignment

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,26 +1,33 @@
 import { useQuery } from "react-query"
 import axios from "axios"
+import { useState } from "react"
 
 const fetchSuperHeroes = () => {
-    return axios.get("http://localhost:4000/superheroes1")
+    return axios.get("http://localhost:4000/superheroes")
 }
 
 export const RQSuperHeroesPage = () => {
+    const [intervalTime, setIntervalTime] = useState(3000)
 
     const onSuccess = (data) => {
+        if (data?.data.length === 4) {
+            setIntervalTime(0)
+        }
         console.log(
             "Perform side effect after data fetching", data
         )
     }
 
     const onError = (data) => {
+        setIntervalTime(0)
         console.log(
             "Perform side effect after encountering error", data
         )
     }
 
     const { isLoading, data, isError, error, isFetching, } = useQuery("super-heroes", fetchSuperHeroes, {
-        onError, onSuccess
+        onError, onSuccess,
+        refetchInterval: intervalTime
     })
     // default cache time is 5 minutes and this 5 minutes is good 
     // default stale time is 0s
