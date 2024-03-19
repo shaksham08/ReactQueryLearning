c963faebebb13cc0c40a69b132188b5ff541552b
shaksham08 <shakshamsinha@outlook.com>
2024-03-19 20:15:46 +0530

    dev:added data modification using select

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -7,27 +7,31 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
-    const [intervalTime, setIntervalTime] = useState(3000)
+    // const [intervalTime, setIntervalTime] = useState(3000)
 
-    const onSuccess = (data) => {
-        if (data?.data.length === 4) {
-            setIntervalTime(0)
-        }
-        console.log(
-            "Perform side effect after data fetching", data
-        )
-    }
+    // const onSuccess = (data) => {
+    //     if (data?.data.length === 4) {
+    //         setIntervalTime(0)
+    //     }
+    //     console.log(
+    //         "Perform side effect after data fetching", data
+    //     )
+    // }
 
-    const onError = (data) => {
-        setIntervalTime(0)
-        console.log(
-            "Perform side effect after encountering error", data
-        )
-    }
+    // const onError = (data) => {
+    //     setIntervalTime(0)
+    //     console.log(
+    //         "Perform side effect after encountering error", data
+    //     )
+    // }
 
     const { isLoading, data, isError, error, isFetching, } = useQuery("super-heroes", fetchSuperHeroes, {
-        onError, onSuccess,
-        refetchInterval: intervalTime
+        // onError, onSuccess,
+        // refetchInterval: intervalTime
+        select: (data) => {
+            const superheroNames = data.data.map(hero => hero.name)
+            return superheroNames
+        }
     })
     // default cache time is 5 minutes and this 5 minutes is good 
     // default stale time is 0s
@@ -48,8 +52,11 @@ export const RQSuperHeroesPage = () => {
     return (
         <>
             <h2>React Query Super Heroes Page</h2>
-            {data?.data.map(hero => {
+            {/* {data?.data.map(hero => {
                 return <div key={hero.name}>{hero.name}</div>
+            })} */}
+            {data.map(heroName => {
+                return <div key={heroName}>{heroName}</div>
             })}
         </>
     )
