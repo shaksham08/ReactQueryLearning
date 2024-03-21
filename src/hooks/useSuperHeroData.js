670d43a0e93ea1d0fee4fc686eadacbadcdfc101
shaksham08 <shakshamsinha@outlook.com>
2024-03-21 22:53:51 +0530

    dev:added initial data to query

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,12 +1,26 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
-const fetchSuperHero = (heroId) => {
+const fetchSuperHero = ({ queryKey }) => {
+  const heroId = queryKey[1];
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(["super-hero", heroId], () => {
-    return fetchSuperHero(heroId);
+  const queryClient = useQueryClient();
+  return useQuery(["super-hero", heroId], fetchSuperHero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData("super-heroes")
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+
+      if (hero) {
+        return {
+          data: hero,
+        };
+      } else {
+        return undefined;
+      }
+    },
   });
 };
