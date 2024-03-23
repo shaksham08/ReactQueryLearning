d99aa0d322af51887827d995f6424acd722d2e20
shaksham08 <shakshamsinha@outlook.com>
2024-03-23 11:23:17 +0530

    dev:mutations in react query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,80 +1,68 @@
-
-
-import { useState } from "react"
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData"
-import { Link } from "react-router-dom"
-
-
+import { useState } from "react";
+import {
+  useAddSuperHeroData,
+  useSuperHeroesData,
+} from "../hooks/useSuperHeroesData";
+import { Link } from "react-router-dom";
 
 export const RQSuperHeroesPage = () => {
-    // const [intervalTime, setIntervalTime] = useState(3000)
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
 
-    // const onSuccess = (data) => {
-    //     if (data?.data.length === 4) {
-    //         setIntervalTime(0)
-    //     }
-    //     console.log(
-    //         "Perform side effect after data fetching", data
-    //     )
-    // }
+  const onSuccess = (data) => {
+    console.log({ data });
+  };
 
-    // const onError = (data) => {
-    //     setIntervalTime(0)
-    //     console.log(
-    //         "Perform side effect after encountering error", data
-    //     )
-    // }
+  const onError = (error) => {
+    console.log({ error });
+  };
 
-    // const { isLoading, data, isError, error, isFetching, } = useQuery("super-heroes", fetchSuperHeroes, {
-    //     // onError, onSuccess,
-    //     // refetchInterval: intervalTime
-    //     select: (data) => {
-    //         const superheroNames = data.data.map(hero => hero.name)
-    //         return superheroNames
-    //     }
-    // })
-    // default cache time is 5 minutes and this 5 minutes is good 
-    // default stale time is 0s
-    // default refetchOnMount = true
-    // refetchOnWindowFocus
-    // refetchInterval
-    // refetchIntervalInBackground
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData(onSuccess, onError);
 
-    const onSuccess = (data) => {
-        console.log(
-            "Perform side effect after data fetching", data
-        )
-    }
+  const { mutate } = useAddSuperHeroData();
 
-    const onError = (data) => {
+  console.log({ isLoading, isFetching });
 
-        console.log(
-            "Perform side effect after encountering error", data
-        )
-    }
+  const handleAddHeroClick = () => {
+    mutate({ name, alterEgo });
+  };
 
-    const { isLoading, isFetching, isError, data, error } = useSuperHeroesData(onSuccess, onError)
-    console.log({ isLoading, isFetching })
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-    if (isLoading) {
-        return <h1>Loading....</h1>
-    }
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
 
-    if (isError) {
-        return <h2>{error.message}</h2>
-    }
-
-    return (
-        <>
-            <h2>React Query Super Heroes Page</h2>
-            {data?.data.map(hero => {
-                return <div key={hero.id}>
-                    <Link to={`/rq-super-heroes/${hero.id}`}>
-                        {hero.name}</Link></div>
-            })}
-            {/* {data.map(heroName => {
-                return <div key={heroName}>{heroName}</div>
-            })} */}
-        </>
-    )
-}
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={alterEgo}
+          onChange={(e) => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
+      <button onClick={refetch}>Fetch heroes</button>
+      {data?.data.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        );
+      })}
+      {/* {data.map(heroName => {
+        return <div key={heroName}>{heroName}</div>
+      })} */}
+    </>
+  );
+};
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,17 +1,25 @@
-import { useQuery } from "react-query"
-import axios from "axios"
+import { useQuery, useMutation } from "react-query";
+import axios from "axios";
 
-const fetchSuperHeroes = (onSuccess, onError) => {
-    return axios.get("http://localhost:4000/superheroes")
-}
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 export const useSuperHeroesData = (onSuccess, onError) => {
-    return useQuery("super-heroes", fetchSuperHeroes, {
-        onSuccess, onError,
-        // select: (data) => {
-        //     const superheroNames = data.data.map(hero => hero.name)
-        //     return superheroNames
-        // }
-    })
+  return useQuery("super-heroes", fetchSuperHeroes, {
+    onSuccess,
+    onError,
+    // select: data => {
+    //   const superHeroNames = data.data.map(hero => hero.name)
+    //   return superHeroNames
+    // }
+  });
+};
 
-}
+const addSuperHero = (hero) => {
+  return axios.post("http://localhost:4000/superheroes", hero);
+};
+
+export const useAddSuperHeroData = () => {
+  return useMutation(addSuperHero);
+};
